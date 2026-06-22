a8517d87167e5165a50e250b100095123e53f6b0
ShohjahonAhmad <shox_0502@mail.ru>
2026-06-22 09:10:24 +0200

    stop tracking .env

--- dist/services/music-generation/convertMidiToWav.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function midiToWav(midiBuffer: Buffer, signal: AbortSignal): Promise<Buffer>;
-//# sourceMappingURL=convertMidiToWav.d.ts.map
--- dist/services/songGenerator.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Song } from '../types/Song.js';
-export declare function generateSpanishSong(seed: bigint, index: number): Song;
-export declare function generateSongs(seed: bigint, page: number, likesNumber: number, locale: string): Song[];
-//# sourceMappingURL=songGenerator.d.ts.map
